ad1b96f600faef0b8b4f09d492afb698017c79b9
Dayane <dayane.lima.castro@gmail.com>
2018-07-15 11:33:25 -0300

    criando tela inicial e algumas imagens para o tutorial

--- CalculadoraFreelancer01/CalculadoraFreelancer01/App.xaml.cs
+++ CalculadoraFreelancer01/CalculadoraFreelancer01/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CalculadoraFreelancer01
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new CalculoValorHoraPage());
 		}
 
 		protected override void OnStart ()
